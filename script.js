72acadd2c08687ebe28723c1a22090909d277340
Saurabh Rawat <saurabhrawat2112000@gmail.com>
2025-10-01 00:19:43 +0530

    products Done

--- script.js
+++ script.js
@@ -48,6 +48,31 @@ document
     const searchCategoryName = tabContent.querySelector(".searchCategoryName");
     const searchCategoryType = tabContent.querySelector(".searchCategoryType");
     const searchMealPlan = tabContent.querySelector(".searchMealPlan");
+    const searchAmenityName = tabContent.querySelector(".searchAmenityName");
+    const searchAmenityType = tabContent.querySelector(".searchAmenityType");
+    const searchPropertyType = tabContent.querySelector(".searchPropertyType");
+    const searchFormulaName = tabContent.querySelector(".searchFormulaName");
+    const searchFormulaType = tabContent.querySelector(".searchFormulaType");
+    const searchHotelGroup = tabContent.querySelector(".searchHotelGroup");
+    const searchDependantID = tabContent.querySelector(".searchDependantID");
+    const searchPromotionID = tabContent.querySelector(".searchPromotionID");
+    const searchCancelPolicyNo = tabContent.querySelector(
+      ".searchCancelPolicyNo"
+    );
+    const searchTransfer = tabContent.querySelector(".searchTransfer");
+    const searchAirportService = tabContent.querySelector(
+      ".searchAirportService"
+    );
+    const searchOtherService = tabContent.querySelector(".searchOtherService");
+    const searchVisa = tabContent.querySelector(".searchVisa");
+    const searchTour = tabContent.querySelector(".searchTour");
+
+    const searchPromotionName = tabContent.querySelector(
+      ".searchPromotionName"
+    );
+    const searchApprovedStatus = tabContent.querySelector(
+      ".searchApprovedStatus"
+    );
 
     const searchHotels = tabContent.querySelector(".searchHotels");
     const searchHotelsGroup = tabContent.querySelector(".searchHotelsGroup");
@@ -101,6 +126,26 @@ document
       const roomClassValue = searchRoomClassName?.value.toLowerCase() || "";
       const categoryTypeValue = searchCategoryType?.value.toLowerCase() || "";
       const mealPlanValue = searchMealPlan?.value.toLowerCase() || "";
+      const amenityNameValue = searchAmenityName?.value.toLowerCase() || "";
+      const amenityTypeValue = searchAmenityType?.value.toLowerCase() || "";
+      const propertyTypeValue = searchPropertyType?.value.toLowerCase() || "";
+      const formulaNameValue = searchFormulaName?.value.toLowerCase() || "";
+      const formulaTypeValue = searchFormulaType?.value.toLowerCase() || "";
+      const hotelGroupValue = searchHotelGroup?.value.toLowerCase() || "";
+      const dependantIDValue = searchDependantID?.value.toLowerCase() || "";
+      const promotionIDValue = searchPromotionID?.value.toLowerCase() || "";
+      const promotionNameValue = searchPromotionName?.value.toLowerCase() || "";
+      const cancelPolicyNoValue =
+        searchCancelPolicyNo?.value.toLowerCase() || "";
+      const transferValue = searchTransfer?.value.toLowerCase() || "";
+      const airportServiceValue =
+        searchAirportService?.value.toLowerCase() || "";
+      const otherServiceValue = searchOtherService?.value.toLowerCase() || "";
+      const visaValue = searchVisa?.value.toLowerCase() || "";
+      const tourValue = searchTour?.value.toLowerCase() || "";
+
+      const approvedStatusValue =
+        searchApprovedStatus?.value.toLowerCase() || "";
 
       const dateFrom = parseDate(dateFromInput?.value);
       const dateTo = parseDate(dateToInput?.value);
@@ -124,15 +169,34 @@ document
         const rowCategoryName = getCellByHeader(row, "category");
         const rowCategoryType = getCellByHeader(row, "category type");
         const rowMealPlan = getCellByHeader(row, "meal plan");
+        const rowPropertyType = getCellByHeader(row, "property type name");
+        const rowFormulaName = getCellByHeader(row, "formula name");
+        const rowFormulaType = getCellByHeader(row, "formula type");
 
         const rowHotels = getCellByHeader(row, "hotel name");
         const rowHotelsGroup = getCellByHeader(row, "hotel chain");
         const rowAgentName = getCellByHeader(row, "agent name");
         const rowCountryGroup = getCellByHeader(row, "country group");
+        const rowHotelGroup = getCellByHeader(row, "hotel group");
+        const rowDependantID = getCellByHeader(row, "dependantid");
+        const rowPromotionID = getCellByHeader(row, "promotionid");
+        const rowPromotionName = getCellByHeader(row, "promotion name");
+        const rowApprovedStatus = getCellByHeader(row, "approved status");
+        const rowCancelPolicyNo = getCellByHeader(
+          row,
+          "other cancel policy no"
+        );
+        const rowTransfer = getCellByHeader(row, "transfer");
+        const rowAirportService = getCellByHeader(row, "airport service");
+        const rowOtherService = getCellByHeader(row, "other service");
+        const rowVisa = getCellByHeader(row, "visa");
+        const rowTour = getCellByHeader(row, "tour");
+
         const rowText = row.innerText.toLowerCase();
         const rowDate = parseDate(getCellByHeader(row, "date created"));
         const rowRoomClassName = getCellByHeader(row, "room class name");
-
+        const rowAmenityName = getCellByHeader(row, "amenity name");
+        const rowAmenityType = getCellByHeader(row, "amenity type");
         // For new table (Currencies Conversion Rate)
         const rowFromCurrency = getCellByHeader(row, "from currency");
         const rowToCurrency = getCellByHeader(row, "to currency");
@@ -162,6 +226,26 @@ document
           (!roomClassValue || rowRoomClassName.includes(roomClassValue)) &&
           (!categoryTypeValue || rowCategoryType.includes(categoryTypeValue)) &&
           (!mealPlanValue || rowMealPlan.includes(mealPlanValue)) &&
+          (!amenityNameValue || rowAmenityName.includes(amenityNameValue)) &&
+          (!amenityTypeValue || rowAmenityType.includes(amenityTypeValue)) &&
+          (!propertyTypeValue || rowPropertyType.includes(propertyTypeValue)) &&
+          (!cancelPolicyNoValue ||
+            rowCancelPolicyNo.includes(cancelPolicyNoValue)) &&
+          (!transferValue || rowTransfer.includes(transferValue)) &&
+          (!airportServiceValue ||
+            rowAirportService.includes(airportServiceValue)) &&
+          (!otherServiceValue || rowOtherService.includes(otherServiceValue)) &&
+          (!visaValue || rowVisa.includes(visaValue)) &&
+          (!tourValue || rowTour.includes(tourValue)) &&
+          (!formulaNameValue || rowFormulaName.includes(formulaNameValue)) &&
+          (!formulaTypeValue || rowFormulaType.includes(formulaTypeValue)) &&
+          (!hotelGroupValue || rowHotelGroup.includes(hotelGroupValue)) &&
+          (!dependantIDValue || rowDependantID.includes(dependantIDValue)) &&
+          (!promotionIDValue || rowPromotionID.includes(promotionIDValue)) &&
+          (!promotionNameValue ||
+            rowPromotionName.includes(promotionNameValue)) &&
+          (!approvedStatusValue ||
+            rowApprovedStatus.includes(approvedStatusValue)) &&
           (!toCurrency || rowToCurrency.includes(toCurrency));
 
         if (match && dateFrom) match = rowDate >= dateFrom;
@@ -184,12 +268,35 @@ document
       searchSupplierType,
       searchSectorName, // 👈 added
       searchSectorGroup,
+      searchFormulaName,
+      searchDependantID,
+      searchPromotionID,
+      searchPromotionName,
+      searchApprovedStatus,
+      searchCancelPolicyNo,
+      searchTransfer,
+      searchAirportService,
+      searchOtherService,
+      searchVisa,
+      searchTour,
+
+      searchFormulaType,
+      searchHotelGroup,
+
       searchCountryGroup, // 👈 added
       fromCurrencySelect,
       searchCategoryName,
       searchRoomClassName,
+      searchAmenityName,
+      searchPropertyType,
+      searchAmenityType,
       searchAgentName,
       searchCategoryType,
+      searchDependantID,
+      searchPromotionID,
+      searchPromotionName,
+      searchApprovedStatus,
+
       searchHotels, // 👈 added
       searchMealPlan,
       searchHotelsGroup,
@@ -212,6 +319,18 @@ document
           searchCurrency,
           searchProvider,
           searchAgentName,
+          searchAmenityName,
+          searchAmenityType,
+          searchFormulaName,
+          searchFormulaType,
+          searchHotelGroup,
+          searchCancelPolicyNo,
+          searchTransfer,
+          searchAirportService,
+          searchOtherService,
+          searchVisa,
+          searchTour,
+
           searchText,
           searchSectorName,
           searchSupplierType, // 👈 added
@@ -220,6 +339,8 @@ document
           fromCurrencySelect,
           searchHotels, // 👈 added
           searchHotelsGroup,
+          searchPropertyType,
+
           searchCategoryType,
           searchMealPlan,
           searchRoomClassName,
